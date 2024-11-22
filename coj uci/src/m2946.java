5e298f1f6411e918cd0dee24c0469f0a381699eb
Jose Barrameda <jagbarrameda@gmail.com>
2024-11-22 14:31:38 -0500

    bs

--- coj uci/src/m2946.java
+++ coj uci/src/m2946.java
@@ -31,24 +31,4 @@ class m2946 {
         }
     }
 
-    public static void main2(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        String line = br.readLine();
-
-        int n = Integer.parseInt(line);
-        int i;
-
-        while (n-- > 0) {
-            line = br.readLine();
-            i = Integer.parseInt(line);
-            if ((i & 1) == 1) i <<= 1;
-            else {
-                line = Integer.toString(i, 2);
-                line = line.substring(0, line.lastIndexOf("1") + 1);
-                i = Integer.parseInt(line, 2);
-            }
-            System.out.println(i);
-        }
-    }
 }
