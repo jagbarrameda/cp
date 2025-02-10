3ee819e3f9ce27cc3d7ac4771f96479069f5b2e5
Jose Barrameda <jagbarrameda@gmail.com>
2025-02-10 14:17:49 -0500

    cf2055c

--- codeforce/src/c2055/c2055c.kt
+++ codeforce/src/c2055/c2055c.kt
@@ -1,15 +1,15 @@
-import kotlin.math.max
+import java.io.PrintWriter
 
 fun main() {
     var t = readln().toInt()
     while (t-- > 0) {
-        val (n, m) = readln().split(" ").map { it.toInt() }
-        val s = readln()
-        val a = Array(n) { readln().split(" ").map { it.toInt() }.toIntArray() }
-        val rowsMisses = IntArray(n)
-        val colsMisses = IntArray(m)
-        var rowMisses = 1
-        var colMisses = 1
+        val (n, m) = C2055C.io.nextInts()
+        val s = C2055C.io.nextLine()
+        val a = Array(n) { C2055C.io.nextLongs().toLongArray() }
+        val rowsMisses = LongArray(n)
+        val colsMisses = LongArray(m)
+        var rowMisses = 1L
+        var colMisses = 1L
         var row = 0
         var col = 0
         for (i in s.indices) {
@@ -28,17 +28,9 @@ fun main() {
         colsMisses[m-1] = colMisses
         rowsMisses[n-1] = rowMisses
 
-        val rowSum=a.map { it.sum() }.toIntArray()
-        val colSum=IntArray(m)
+        val rowSum=a.map { it.sum() }.toLongArray()
+        val colSum=LongArray(m)
         for (col in 0 until m) { for (row in 0 until n) { colSum[col] += a[row][col] } }
-
-        var x = 0 // candidate
-//        for (row in 0 until n) {
-//            if (rowsMisses[row] == 1) x = max(x, rowSum[row])
-//        }
-//        for (col in 0 until m) {
-//            if (colsMisses[col] == 1) x = max(x, colsMisses[col])
-//        }
 
         // x is set
         // mark path
@@ -49,15 +41,15 @@ fun main() {
             col = 0
             var i = 0
             while (row < n && col < m) {
-                if (rowsMisses[row] == 1) {
-                    a[row][col] = x - rowSum[row]
+                if (rowsMisses[row] == 1L) {
+                    a[row][col] = -rowSum[row]
                     rowSum[row]+=a[row][col]
                     colSum[col]+=a[row][col]
                     rowsMisses[row]--
                     colsMisses[col]--
                     changed = true
-                } else if (colsMisses[col] == 1) {
-                    a[row][col] = x - colSum[col]
+                } else if (colsMisses[col] == 1L) {
+                    a[row][col] = -colSum[col]
                     rowSum[row]+=a[row][col]
                     colSum[col]+=a[row][col]
                     rowsMisses[row]--
@@ -75,7 +67,28 @@ fun main() {
             }
         }
         for (row in 0 until n) {
-            println(a[row].joinToString(" "))
+            C2055C.io.writeLine(a[row].joinToString(" "))
         }
+    }
+    C2055C.io.flush()
+}
+
+object C2055C {
+    object io {
+        val cin = System.`in`.bufferedReader()
+        val cout = PrintWriter(System.out.bufferedWriter())
+        val endl = "\n"
+        val space = " "
+        fun nextLine() = cin.readLine()!!.trim()
+        fun nextInt() = nextLine().toInt()
+        fun nextLong() = nextLine().toLong()
+        fun nextStrings() = nextLine().split(space)
+        fun nextInts() = nextStrings().map { it.toInt() }
+        fun nextLongs() = nextStrings().map { it.toLong() }
+        fun write(a: Int) = cout.write(a.toString() + space)
+        fun write(s: String) = cout.write(s)
+        fun writeLine(s: String) = cout.write("$s$endl")
+        fun flush() = cout.flush()
+        fun done() = cout.flush()
     }
 }
